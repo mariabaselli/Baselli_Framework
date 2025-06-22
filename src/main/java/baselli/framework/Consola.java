370d21af65873ce58998745d62efa4c1d6b54c79
mariabaselli <mariangelesbaselli@hotmail.com>
2025-06-22 09:55:16 -0300

    Refactor

--- src/main/java/baselli/framework/Consola.java
+++ src/main/java/baselli/framework/Consola.java
@@ -17,11 +17,8 @@ public class Consola {
     public static final String SPLIT = ",";
     public static final String SPACE = " ";
     private Accion menu;
-   private List<Accion> acciones= new ArrayList<>();
+    private List<Accion> acciones = new ArrayList<>();
 
-    public Consola() {
-
-    }
 
     public Consola(String pathConfiguracion) {
         Properties prop = new Properties();
@@ -42,36 +39,45 @@ public class Consola {
 
 
     public void imprimir() {
-        int option =1;
         int indice = 1;
         Scanner scanner = new Scanner(System.in);
+        desplegarMenu(indice, scanner);
+    }
+
+    private void desplegarMenu(int indice, Scanner scanner) {
+        int option;
         System.out.println(MENSAJE_BIENVENIDA);
 
-            while (true) {
-                System.out.println(MENÚ);
-                for (Accion menu : acciones) {
-                    System.out.println(indice + ".- " + menu.nombreItemMenu() + menu.descripcionItemMenu());
-                    indice += 1;
-                }
-                System.out.println(indice + OPCION_SALIR);
-                indice = 1;
-                System.out.print("Indique a continuación su opcion del " + indice + "-" + (acciones.size()+1) + ": ");
-
-                option = scanner.nextInt();
-                System.out.println(SPACE);
-                if (option >= 1 && option <= acciones.size()) {
-                    acciones.get(option - 1).ejecutar();
-                } else if (option == acciones.size()+1) {
-                    System.out.println(MENSAJE_ÉXITO);
-                    break;
-                }
-                else{
-                    System.out.println(MENSAJE_ERROR);
-                }
-
-                System.out.println(SPACE);
+        while (true) {
+            System.out.println(MENÚ);
+            for (Accion menu : acciones) {
+                System.out.println(indice + ".- " + menu.nombreItemMenu() + menu.descripcionItemMenu());
+                indice += 1;
             }
+            System.out.println(indice + OPCION_SALIR);
+            indice = 1;
+            System.out.print("Indique a continuación su opcion del " + indice + "-" + (acciones.size() + 1) + ": ");
 
-    }
+            option = scanner.nextInt();
+            System.out.println(SPACE);
+            if (opcionContinuar(option)) {
+                acciones.get(option - 1).ejecutar();
+            } else if (opcionSalir(option)) {
+                System.out.println(MENSAJE_ÉXITO);
+                break;
+            } else {
+                System.out.println(MENSAJE_ERROR);
+            }
+            System.out.println(SPACE);
         }
+    }
+
+    private boolean opcionSalir(int option) {
+        return option == acciones.size() + 1;
+    }
+
+    private boolean opcionContinuar(int option) {
+        return option >= 1 && option <= acciones.size();
+    }
+}
 
